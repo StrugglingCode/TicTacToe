aa926c11cdc5b386ada5b7718f70de86ec8f6c9a
Rishu Garg <rishugarg102@gmail.com>
2019-06-17 13:26:20 +0530

    Created function ImageButtonTapped
    icons from flaticon.com

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.tictactoe
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun imageButtonTapped(view:View)
+    {
+        val selectedImageButton:ImageButton = view as ImageButton
+
+        when(selectedImageButton.id)
+        {
+            R.id.imgButton1 -> tableLayout.setBackgroundColor(Color.BLACK)
+        }
     }
 }
