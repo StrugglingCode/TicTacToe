7bbbab52c6f797d5e53b6e5d871ca3225a411e87
Rishu Garg <rishugarg102@gmail.com>
2019-06-17 13:34:26 +0530

    Update MainActivity.kt
    Random Color Generation

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -17,10 +17,24 @@ class MainActivity : AppCompatActivity() {
     fun imageButtonTapped(view:View)
     {
         val selectedImageButton:ImageButton = view as ImageButton
-
-        when(selectedImageButton.id)
+        var randomNumber = (Math.random()* 9 + 1).toInt()
+        //Math.random() generates value b/w 0&1
+        //randomNumber generated b/w 1-9 including 9
+        when(randomNumber)
         {
-            R.id.imgButton1 -> tableLayout.setBackgroundColor(Color.BLACK)
+            1-> tableLayout.setBackgroundColor(Color.GREEN)
+            2-> tableLayout.setBackgroundColor(Color.RED)
+            3-> tableLayout.setBackgroundColor(Color.YELLOW)
+            4-> tableLayout.setBackgroundColor(Color.CYAN)
+            5-> tableLayout.setBackgroundColor(Color.WHITE)
+            6-> tableLayout.setBackgroundColor(Color.DKGRAY)
+            7-> tableLayout.setBackgroundColor(Color.LTGRAY)
+            8-> tableLayout.setBackgroundColor(Color.BLUE)
+            9-> tableLayout.setBackgroundColor(Color.MAGENTA)
+
+
+
         }
+
     }
 }
